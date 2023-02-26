8ce824559012f8ed96e4857509acb78964b2b7da
Zeddnyx <zeddnyx08@gmail.com>
2023-02-26 10:47:29 +0700

    add cc gen but didint finish yet

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Chalange from frontendmentor
 
--- src/Pages/BinGen.jsx
+++ src/Pages/BinGen.jsx
@@ -1,80 +1,110 @@
 import { Link } from 'react-router-dom'
+import { useReducer, useState } from 'react'
 
 export default function CCGEN() {
+  const [month, setMonth] = useState(
+    ['random','01','02','03','04','05','06','07','08','09','10','11','12']
+  )
+  const [year, setYear] = useState(
+    ['random','2023','2024','2025','2026','2027','2028','2029','2030','2031']
+  )
 
-  const handleSubmit= e => {
-    e.preventDefault()
+  const [results, setResults] = useState({})
+
+  const initialState = {
+    bin: 0,
+    date: false,
+    month: 'random',
+    year: 'random',
+    cvv: 0,
+    cvvCheck: false,
+    quantity: 0,
+    type: 'pipe'
   }
-  return <> 
+  const postRedcuer = (state, action) => {
+    switch (action.type) {
+      case 'INPUT':
+        return {
+          ...state, [action.payload.name]: action.payload.value
+        }
+      case 'DATE':
+        return {
+          ...state, date: !state.date
+        }
+      case 'CVV':
+        return {
+          ...state, cvvCheck: !state.cvvCheck
+        }
+      default:
+        return state
+    }
+  }
+  const [state, dispatch] = useReducer(postRedcuer, initialState)
+
+  const handleResults = e => {
+    dispatch({ type: 'INPUT', payload: {name: e.target.name, value: e.target.value} })
+    setResults(state)
+    console.log(results)
+  }
+
+  return <>
     <div className='my-20'>
       <Link to='/'>Back</Link>
     </div>
     <section className='max-w-sm mx-auto'>
-      <form onSubmit={handleSubmit}>
+      <form>
       <div className='grid gap-5'>
         <fieldset className='field'>
-          <legend className='px-3'>Bin</legend>
-          <input className='input' type="number" name="bin" placeholder='454393' />
+          <legend className='legend'>Bin</legend>
+          <input  className='input' type="number" onChange={handleResults} name="bin" placeholder='454393' />
         </fieldset>
 
-        <div className='flex gap-5'>
+        <div className='flex gap-3 font-pop'>
           <div className='flex gap-5 items-center'>
-            <input className='check' type="checkbox" name="date" />
+            <input onClick={() => dispatch({ type: 'CHECK' })} className='check' type="checkbox" name="date" />
             <label htmlFor="date">Date</label>
           </div>
 
           <fieldset className='field'>
-             <legend className='px-3'>MONTH</legend>
-             <select className='select' name="month">
-               <option value="random">Random</option>
-               <option value="01">January</option>
-               <option value="02">February</option>
-               <option value="03">March</option>
-               <option value="04">April</option>
-               <option value="05">May</option>
-               <option value="06">June</option>
-               <option value="07">July</option>
-               <option value="08">August</option>
-               <option value="09">September</option>
-               <option value="10">October</option>
-               <option value="11">November</option>
-               <option value="12">December</option>
+             <legend className='legend'>MONTH</legend>
+             <select className='select'  onChange={handleResults} name="month">
+               {month.map( month => {
+                 return <option key={month} value={month}>{month}</option>
+               })}
              </select>
           </fieldset>
 
           <fieldset className='field'>
-            <legend className='px-3'>Year</legend>
-             <select className='select' name="year">
-            	<option value="random">Random</option>
-            	<option value="2023">2023</option>
-            	<option value="2024">2024</option>
-            	<option value="2025">2025</option>
-            	<option value="2026">2026</option>
-            	<option value="2027">2027</option>
-            	<option value="2028">2028</option>
-            	<option value="2029">2029</option>
-            	<option value="2030">2030</option>
-            	<option value="2031">2031</option>
+            <legend className='legend'>Year</legend>
+             <select className='select'  onChange={handleResults} name="year">
+               {year.map( year => {
+                 return <option key={year} value={year}>{year}</option>
+               })}
             </select>
           </fieldset>
         </div>
 
         
         <div className='flex gap-5'>
+          <div className='flex gap-5 items-center'>
+            <input onClick={() => dispatch({ type: 'CVV' })} className='check' type="checkbox" name="cvvCheck" />
+            <label htmlFor="date">Cvv</label>
+          </div>
+
           <fieldset className='field'>
-            <legend className='px-3'>CVV</legend>
-            <input className='input' type="number" name="bin" placeholder='Leave blank to random' />
+            <legend className='legend'>CVV</legend>
+            <input  onChange={handleResults} className='input' type="number" name="cvv" placeholder='Leave blank to random' />
           </fieldset>
             
           <fieldset className='field'>
-            <legend className='px-3'>Quantity</legend>
-            <input className='input' type="number" name="bin" placeholder='10' />
+            <legend className='legend'>Quantity</legend>
+            <input  onChange={handleResults} className='input' type="number" name="quantity" placeholder='10' />
           </fieldset>
         </div>
 
         <fieldset className='field'>
-          <legend className='px-3'>Pipe</legend>
-          <select className='select' name="year">
+          <legend className='legend'>Pipe</legend>
+          <select onChange={handleResults} className='select' name="type">
             <option value="pipe">PIPE</option>
             <option value="json">JSON</option>
             <option value="csv">CSV</option>
@@ -85,7 +115,8 @@ export default function CCGEN() {
       </div>
       </form>
 
-      <textarea className='textarea'></textarea>
+      <div className='textarea'>
+      </div>
     </section>
   </>
 }
