972aafef319110a6ee912ba3fc518df65349ff2b
Zeddnyx <zeddnyx08@gmail.com>
2023-02-27 08:35:19 +0700

    add validator bin

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -4,15 +4,15 @@ export default function Home() {
   return <>
     <section className='grid grid-cols-2 gap-5'>
       <Link to='tools1'>
-        <div className='boxTools'>
+        <button className='boxTools'>
            <h1>Fitur satu</h1>
-        </div>
+        </button>
       </Link>
 
       <Link to='bingenerator'>
-        <div className='boxTools'>
+        <button className='boxTools'>
           <h1>Bin Generator</h1>
-        </div>
+        </button>
       </Link>
     </section>
   </>
