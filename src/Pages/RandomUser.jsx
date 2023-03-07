d230236188e6adc2fe274363ff29a1c9a0d1a9ae
Zeddnyx <zeddnyx08@gmail.com>
2023-03-07 13:01:34 +0700

    update randomuser

--- src/Pages/RandomUser.jsx
+++ src/Pages/RandomUser.jsx
@@ -3,11 +3,13 @@ import Card from '../Components/RandomUser/Card'
 
 export default function RandomUser() {
   const [datas, setDatas] = useState([])
+  const [quantity, setQuantity] = useState(5)
 
   const user = async () => {
     const data = await fetch('https://randomuser.me/api/')
     try {
       const json = await data.json()
+      // if just like this setDatas(json.results[0]) will be error datas is not func
       setDatas([json.results[0]])
       console.log(datas)
     } catch (err) {
@@ -18,15 +20,18 @@ export default function RandomUser() {
     user()
   }, [])
 
+  const handle = e => e.preventDefault()
+  const change = e => setQuantity(e.target.value)
+
   return <section className='section'>
     <h1 className='heading-judul'>Generate random user</h1>
     <div className='mt-20'>
       <div className='mb-10'>
         <fieldset className='field'>
           <legend className='legend'>quantity</legend>
-          <input className='input' type="text" name="quantity" />
+          <input className='input' type="number" onChange={change} name="quantity" placeholder='5' />
         </fieldset>
-        <button className='btn' type="submit">Generate</button>
+        <button className='btn' onClick={handle} type="submit">Generate</button>
       </div>
 
 
