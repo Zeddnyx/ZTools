e2386fee697b3c1a80ed07d63b3b8bec7b5c6ea1
Zeddnyx <zeddnyx08@gmail.com>
2023-08-13 10:29:43 +0700

    style: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ZTools",
   description: "A collection of useful tools for your daily needs",
+  icons: {
+    icon: "/favicon.ico"
+  }
 };
 
 export default function RootLayout({
